bed679756738ebf5faaded371a6f9e6a778a361f
icing <icing@penguin>
2023-09-11 19:48:45 +0800

    first commit

---
+++ DataStruct/DataStruct/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace DataStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = 3.14;
+            long c = 1234567890;
+            float d = 3.14f;
+            string e = "Hello World";
+            bool f = true;
+            char g = 'A';
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+            Console.WriteLine("c = {0}", c);
+            Console.WriteLine("d = {0}", d);
+            Console.WriteLine("e = {0}", e);
+            Console.WriteLine("f = {0}", f);
+            Console.WriteLine("g = {0}", g);
+        }
+    }
+}
